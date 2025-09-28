fe9495fc032f43426e0cad9d29cc689749ee9c95
mifta <miftakhul348@gmail.com>
2025-09-28 18:52:35 +0700

    add .jar library

--- android/app/src/main/java/io/ionic/starter/rfidPlugin/RfidReader.java
+++ android/app/src/main/java/io/ionic/starter/rfidPlugin/RfidReader.java
@@ -4,10 +4,11 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.Plugin;
+import com.payne.connect.otg.UsbHandle;
 
 @CapacitorPlugin(name = "RfidReader")
 public class RfidReader extends Plugin {
-
+private UsbHandle usbHandle;
     @PluginMethod()
     public void helloWorld(PluginCall call) {
         JSObject ret = new JSObject();
